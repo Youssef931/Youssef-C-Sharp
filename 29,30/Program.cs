ed079c8f99e3df8d07033c1553e8c944681fa6ac
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-08-17 14:17:09 +0200

    Increments - Decrements Operations & Assignment Operations
    Nazar<<Alwattar<<C#<<29,30;

---
+++ 29,30/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace _29_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int myVar1 = 2;
+            //int temp1 = myVar1--;
+            //Console.WriteLine("{0}", temp1);
+            //***************************************************
+            //Q1 - 5;
+            //***************************************************
+            //Q2 - 6;
+            //***************************************************
+            //Q3 - int myvar1 = 7;
+            //int myvar2 = 2;
+            //int temp1 = myvar1-- / ++myvar2;
+            //Q3 - 2.3, لأن الفار1 لم يتغير لأن العلامات جائت بعده أما الفار2 العكس;
+            //Console.WriteLine("{0}", temp1);
+            //***************************************************
+            //int myVar1 = 7, myVar2 = 3;
+            ////myVar2 = myVar1;
+            ////myVar1 += myVar2;
+            //myVar1 -= myVar2;
+            //Console.WriteLine("R is {0}\nR2 is {1}", myVar1,myVar2);
+            //***************************************************
+            //float myvar1 = 7, myvar2 = 2;
+            //float temp1 = myvar1 /= myvar2;
+            //Q1 - 3.5
+            //Console.WriteLine("{0}", temp1);
+        }
+    }
+}
