0e68cd9a2ec889b6ec539690eb0872ec96cedc4a
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-08-18 08:48:18 +0200

    If Statment
    Nazar<<Alwattar<<C#<<33;

---
+++ If Statment/If Statment/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace If_Statment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - int myVar = 10;
+            //if (myVar == 10)
+            //{
+            //    Console.WriteLine("My Variable is equal 10");
+            //}
+            //*********************************************
+            //2 - int myVar = 10;
+            //if (myVar == 10)
+            //    Console.WriteLine("My Variable is equal 10");
+            //else
+            //    Console.WriteLine("My var is not equal 10");
+            //*********************************************
+            //3 - int myVar = 10;
+            //if (myVar == 10)
+            //{
+            //    myVar = 11;
+            //    Console.WriteLine("{0}", myVar);
+            //    Console.WriteLine("My Variable is equal 10");
+            //}
+            //else
+            //    Console.WriteLine("My var is not equal 10");
+            //*********************************************
+            //4 - int myVar1 = 9, myVar2 = 7;
+            //if ((myVar1 <= 9) && (myVar2 >= 7))
+            //{
+            //    Console.WriteLine("The condetions are True");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The condetions are False");
+            //}
+            //*********************************************
+            //5 - int myVar1 = 9, myVar2 = 7;
+            //if ((myVar1 <= 7) && (myVar2 >= 10))
+            //{
+            //    Console.WriteLine("The condetions are True");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The condetions are False");
+            //}
+            //*********************************************
+            //6 - int myVar = 10;
+            //string myResult;
+            //if (myVar < 10)
+            //{
+            //    myResult = "Less Than 10";
+            //    Console.WriteLine(myResult);
+            //}
+            //if (myVar > 10)
+            //{
+            //    myResult = "Greater Than 10";
+            //    Console.WriteLine(myResult);
+            //}
+            //if (myVar == 10)
+            //{
+            //    myResult = "Equal 10";
+            //    Console.WriteLine(myResult);
+            //*********************************************
+            //Q1 - int myVar1 = 8;
+            //if (myVar1 > 10 )
+            //{
+            //    Console.WriteLine("My Var is Greater Than 10");
+            //}
+            //if (myVar1 < 10)
+            //{
+            //    Console.WriteLine("My Var is less Than 10");
+            //}
+            //*********************************************
+           //Q2 - int myVar1 = 10, myVar2 = 5, myVar3 = 9;
+           //1 - if ((myVar1 <= 11) || (myVar2 == 5) && (myVar3 >= 8)) 
+           // Console.WriteLine("The Conditions are True");
+           // else
+           //  Console.WriteLine("The Conditions are False");
+            //Q2  - 1 - T;
+            //*********************************************
+            //Q2 - int myVar1 = 10, myVar2 = 5, myVar3 = 9;
+            //2 - if ((myVar1 <= 10)||(myVar2 == 6) && (myVar3 >= 8))
+            //{
+            //    Console.WriteLine("The Conditions are True");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The Conditions are False");
+            //}
+            //Q2 - 2 -  T;
+        }
+    }
+}
