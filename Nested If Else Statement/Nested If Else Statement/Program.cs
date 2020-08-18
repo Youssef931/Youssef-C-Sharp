13e9c1db9f788d39747f3fc6bba957865a2dcb44
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-08-18 09:30:33 +0200

    Nested If Else Statment
    Nazar<<Alwattar<<C#<<34,35;

---
+++ Nested If Else Statement/Nested If Else Statement/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Nested_If_Else_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - int myVar1 = 9, Myvar2 = 6;
+            //if (myVar1 == 9)
+            //{
+            //    Console.WriteLine("myVar1 == 9");
+            //    if (Myvar2 == 7)
+            //    {
+            //        Console.WriteLine("myVar2 == 7");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("myVar2 != 7");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("myVar1 != 9");
+            //}
+            //*********************************************
+            //2 - int myVar1 = 9, Myvar2 = 6;
+            //if (myVar1 == 10)
+            //{
+            //    Console.WriteLine("myVar1 == 9");
+            //    if (Myvar2 == 7)
+            //    {
+            //        Console.WriteLine("myVar2 == 7");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("myVar2 != 7");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("myVar1 != 9");
+            //}
+            //*********************************************
+            //3 - int myVar1 = 4, myVar2 = 6;
+            //if (myVar1 <= 3 && myVar2 >= 5) 
+            //{
+            //    Console.WriteLine("Condition One is right");
+            //}
+            //else if (myVar1 <= 4 && myVar2 >= 4) 
+            //{
+            //    Console.WriteLine("Condition Two is right");
+            //}
+            //else if (myVar2 > 5) 
+            //{
+            //    Console.WriteLine("Condition Three is right");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Condition Four is right");
+            //}
+            //*********************************************
+            //Q1 - int myVar1 = 10, myVar2 = 6, myVar3 = 8;
+            //1 - if(myVar1 == 10)
+            //{
+            //    Console.WriteLine("myVar1 == 10");
+            //2 -  if ((myVar2 >= 5)&&(myVar3 <= 9))
+            //    {
+            //        Console.WriteLine("The condetions Are True");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("The condetions Are False");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("myVar1 != 10");
+            //}
+            //Q1 - 1 - T;
+            //Q1 - 2 - T; 
+
+        }
+    }
+}
