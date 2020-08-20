6864728da916bb88381cf70b2a3b528b64335888
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-08-20 08:38:31 +0200

    Switch Statement
    Nazar<<Alwattar<<C#<<36;

---
+++ Switch Statement/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Switch_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - string myAnimal = "Cat";
+            //switch (myAnimal)
+            //{
+            //    case "Canary":
+            //        Console.WriteLine("Canary");
+            //        break;
+            //    case "Cat":
+            //        Console.WriteLine("Cat");
+            //        break;
+            //    case "Camel":
+            //        Console.WriteLine("Camel");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No one");
+            //        break;
+            //}
+            //*******************************************
+            //2 - int myVar = 15;
+            //switch (myVar)
+            //{
+            //    case 10:
+            //        Console.WriteLine("10");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("7");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("14");
+            //        break;
+            //    default:
+            //        Console.WriteLine("no one");
+            //        break;
+            //}
+            //*******************************************
+            //3 - int myVar = 15;
+            //switch (myVar)
+            //{
+            //    case 10:
+            //        Console.WriteLine("10");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("7");
+            //        break;
+            //    case 15:
+            //        Console.WriteLine("15");
+            //        break;
+            //    default:
+            //        Console.WriteLine("no one");
+            //        break;
+            //}
+            //*******************************************
+            //Q1 - string WhatHeSay = "Hello";
+            //switch (WhatHeSay)
+            //{
+            //    case "Hello":
+            //        Console.WriteLine("Hello");
+            //        break;
+            //    case "Bye":
+            //        Console.WriteLine("Bye");
+            //        break;
+            //    case "Welcome":
+            //        Console.WriteLine("Welcome");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No Word");
+            //        break;
+            //        //Q1 - Hello;
+            //}
+        }
+    }
+}
