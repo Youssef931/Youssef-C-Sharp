96d0357544430359caa024240e512d6ab956d278
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-08-20 08:53:40 +0200

    Tenary Condition
    Nazar<<Alwattar<<C#<<38;

---
+++ Ternary Condition/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Ternary_Condition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - int myVar = 9;
+            //string myResult = (myVar < 10) ? "Less Than 10" : "More Than 10";
+            //Console.WriteLine("My R is {0}", myResult);
+            //*********************************************
+            //2 - int myVar = 9;
+            //string myResult = (myVar < 8) ? "Less Than 8" : "More Than 8";
+            //Console.WriteLine("My R is {0}", myResult);
+            //*********************************************
+            //3 - string myName = "Youssef", Result;
+            //Result = (myName.Equals("Youssef") ? "Yes" : "No");
+            //Console.WriteLine("My V R is {0}", Result);
+            //*********************************************
+            //4 - string myName = "youssef", Result;
+            //Result = (myName.Equals("Youssef") ? "Yes" : "No");
+            //Console.WriteLine("My V R is {0}", Result);
+        }
+    }
+}
