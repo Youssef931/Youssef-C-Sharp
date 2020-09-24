714a2fafccfd11e8d8504f154b911af60edf8589
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-09-24 06:39:21 +0200

    Nested loop
    Nazar<<Alwttar<<C#;

---
+++ Nested loop.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Nested_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine("Hello Youssef");
+            //    for (int j = 0;  j < 2;  j++)
+            //    {
+            //        Console.WriteLine("\t Hello world");
+            //    }
+            //    Console.WriteLine("\n");
+            //}
+            //***************************************************
+            //Q1 - for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 0; j <= 10; j++)
+            //    {
+            //        Console.WriteLine("{0} * {1} = {2}", i, j, i * j);
+            //    }               
+            //}
+            //***************************************************
+            //Q2 - for (int i = 0; i <= 6; i+=2)
+            //{
+            //    for (int j = 0; j < 10; j++)
+            //    {
+            //        Console.WriteLine("{0} * {1} = {2}", i, j, i * j);
+            //    }
+            //}
+            //***************************************************
+            //Q3 - for (int i = 1; i <= 6; i+=2)
+            //{
+            //    for (int j = 0; j <= 10; j++)
+            //    {
+            //        Console.WriteLine("{0} * {1} = {2}", i, j, i * j);
+            //    }
+           // }
+            }
+        }
+    }
