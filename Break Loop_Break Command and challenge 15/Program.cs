da9637f2417372fa561ccb49f5831437bfe4c004
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-09-27 06:18:53 +0200

    Break Loop_Break Command and challenge 15
    Nazar<<Alwattar<<C#<<43;

---
+++ Break Loop_Break Command and challenge 15/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Break_Loop_Break_Command_and_challenge_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - for (int i = 0; i <= 5; i++)
+            //{
+            //    Console.WriteLine("Hello Nazar");
+            //    if (i > 2)
+            //        break;
+            //    for (int j = 0; j <= 4; j++)
+            //    {
+            //        Console.WriteLine("\t Hello World");
+            //    }
+            //    Console.WriteLine("\n");
+            //*****************************************
+            //2 - for (int i = 0; i <= 5; i++)
+            //{
+            //    Console.WriteLine("Hello Nazar");
+            //    //if (i > 2)
+            //        //break;
+            //    for (int j = 0; j <= 4; j++)
+            //    {
+            //        Console.WriteLine("\t Hello World");
+            //   }
+            //}
+            //    Console.WriteLine("\n");
+            //*****************************************
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i == 7)
+                    break;
+                for (int j = 0; j <= 10; j++)
+                {
+                    Console.WriteLine("{0} * {1} = {2}", i, j, i * j);
+                }
+                Console.WriteLine("--------------------------");
+            }
+        }
+    }
+}
