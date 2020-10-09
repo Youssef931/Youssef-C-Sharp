7f2d6c3b754c3fcc4c556f57880a61bad6e2fda2
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-10-10 00:51:27 +0200

    44 - Break Loop _ Continue Command and challenge 16
    Nazar<<Alwattar<<C#;

---
+++ Break Loop _ Continue Command and challenge 16/Break Loop _ Continue Command and challenge 16/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Break_Loop___Continue_Command_and_challenge_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - for (int i = 0; i <= 5; i++)
+            //{
+            //   Console.WriteLine("Hello Nazar");
+            //    if (i > 2)
+            //        continue;
+            //    for (int j = 0; j <= 4; j++)
+            //    {
+            //        Console.WriteLine("\tHello world");
+            //    }
+            //     Console.WriteLine("\n");
+            //*********************************************
+            //Q1 - for (int i = 1; i <= 10; i++)
+            //{
+            //    if (i == 7)
+            //        continue;
+            //    for (int j = 0; j <= 10; j++)
+            //    {
+            //        Console.WriteLine("{0} * {1} = {2}", i, j, i * j);
+            //    }
+            //    Console.WriteLine("--------------------------");
+            }
+        }
+    }
