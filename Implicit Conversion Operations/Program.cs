64593254fd58a57a842964e2766ff3d3c6832fc0
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-10-11 00:16:36 +0200

    Implicit Conversion Operations
    Nazar<<Alwattar<<C#<<45;

---
+++ Implicit Conversion Operations/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Implicit_Conversion_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - long myvar = 1000;
+            //float myvar1 = myvar;
+            //Console.WriteLine($"{myvar1}");
+            //*********************************************
+            char myvar = 'G';
+            int myvar1 = myvar;
+            Console.WriteLine($"{myvar1}");
+        }
+    }
+}
