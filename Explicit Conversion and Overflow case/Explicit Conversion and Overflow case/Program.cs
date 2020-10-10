06e693f991db64f64765087948b612455bddf80a
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-10-11 00:48:10 +0200

    Explicit Conversion and Overflow case
    Nazar<<Alwatttar<<C#<<46;

---
+++ Explicit Conversion and Overflow case/Explicit Conversion and Overflow case/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Explicit_Conversion_and_Overflow_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - byte mySbyte;
+            //int myint = 7;
+            //mySbyte = (byte)myint;
+            //Console.WriteLine($"My variable int = {myint}" + "\n");
+            //Console.WriteLine($"My  sbyte  = {mySbyte}");
+            //*********************************************
+            //2 - byte mySbyte;
+            //int myint = 350;
+            //mySbyte = (byte)myint;
+            //Console.WriteLine($"My variable int = {myint}" + "\n");
+            //Console.WriteLine($"My  sbyte  = {mySbyte}");
+        }
+    }
+}
