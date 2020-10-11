7f6cfdd9894176a28ab30f916e8f292b7283289f
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-10-12 00:49:52 +0200

    Checked and Unchecked and Challenge 17
    Nazar<<Alwattar<<C#<<47;

---
+++ Checked and Unchecked and Challenge 17/Checked and Unchecked and Challenge 17/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Checked_and_Unchecked_and_Challenge_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - byte mySbyte;
+            //int myInt = 350;
+            //mySbyte = checked((byte)myInt);
+            //Console.WriteLine($"My variable int = {myInt}" + "\n");
+            //Console.WriteLine($"My byte variable = {mySbyte}");
+            //*********************************************
+            //2 - byte mySbyte;
+            //int myInt = 350;
+            //mySbyte = unchecked((byte)myInt);
+            //Console.WriteLine($"My variable int = {myInt}" + "\n");
+            //Console.WriteLine($"My byte variable = {mySbyte}");
+            //*********************************************
+            //Q1 - decimal myvar1 = 450000000000000000;
+            //int myvar2 = unchecked((int)myvar1);
+            //Console.WriteLine($"My variable decimal = {myvar1}" + "\n");
+            //Console.WriteLine($"My variable int = {myvar2}");
+        }
+    }
+}
