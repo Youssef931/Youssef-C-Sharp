47ec718894f7656ef8164b5b220fb087a3e5cd11
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-10-13 01:12:00 +0200

    Convert Command and Challenge 18
    Nazar<<Alwattat<<C#<<48;

---
+++ Convert Command and Challenge 18/Convert Command and Challenge 18/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Convert_Command_and_Challenge_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - double myDoubleVar = 2500.45;
+            //string myStringVar, mySubstring;
+            //myStringVar = Convert.ToString(myDoubleVar);
+            //Console.WriteLine("My String var is {0}\n", myStringVar);
+            //mySubstring = myStringVar.Substring(0, 2);
+            //Console.WriteLine($"My variable is {myDoubleVar}\n");
+            //Console.WriteLine("My String var is {0}\n", mySubstring);
+            //*********************************************
+            //2 - double myDoubleVar = 2500.45;
+            //string myStringVar, mySubstring;
+            //myStringVar = Convert.ToString(myDoubleVar);
+            //Console.WriteLine("My String var is {0}\n", myStringVar);
+            //mySubstring = myStringVar.Substring(0, 2);
+            //Console.WriteLine($"My variable is {myDoubleVar}\n");
+            ////Console.WriteLine("My String var is {0}\n", mySubstring);
+            //*********************************************
+            float myFlaovar = 45.56f;
+            string myStrivar, mysubStrivar;
+            myStrivar = Convert.ToString(myFlaovar);
+            Console.WriteLine($"my Stri var is {myStrivar}\n");
+            mysubStrivar = myStrivar.Substring(2,3);
+            Console.WriteLine("My floa var is {0}\n", myFlaovar);
+            Console.WriteLine("my Stri var is {0}\n", mysubStrivar);
+        }
+    }
+}
