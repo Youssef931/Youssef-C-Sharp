66d60dd187b62ed1083c48ede8f24376cd69ae78
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-10-15 01:28:09 +0200

    Input Methods In C Sharp and Challenge 19
    Nazar<<Alwattar<<C#<<49;

---
+++ Input Methods In C Sharp and Challenge 19/Input Methods In C Sharp and Challenge 19/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Input_Methods_In_C_Sharp_and_Challenge_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - string MyName;
+            //int MyVar1,MyVar2;
+            //Console.WriteLine("Please Enter Your Name : ");
+            //MyName = Console.ReadLine();
+            //Console.WriteLine($"Hello {MyName}");
+            //*********************************************
+            //2 - string MyName, myStrNumber;
+            //int MyVar1, MyVar2;
+            //Console.WriteLine("Please Enter Your Name : ");
+            //MyName = Console.ReadLine();
+            //Console.WriteLine("Please Enter Your First Number : ");
+            //myStrNumber = Console.ReadLine();
+            //MyVar1 = Convert.ToInt32(myStrNumber);
+            //Console.WriteLine($"Hello {MyName}");
+            //Console.WriteLine($"You Enterd Number {MyVar1}");
+            //***********************************************
+            //3 - string MyName;
+            //int MyVar1, MyVar2;
+            //Console.WriteLine("Please Enter Your Name : ");
+            //MyName = Console.ReadLine();
+            //Console.WriteLine("Please Enter Your First Number : ");
+            //MyVar1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Hello {MyName}");
+            //Console.WriteLine($"You Enterd Number {MyVar1}");
+            //*********************************************
+            //4 - string MyName;
+            //int MyVar1, MyVar2;
+            //Console.WriteLine("Please Enter Your Name : ");
+            //MyName = Console.ReadLine();
+            //Console.WriteLine("Please Enter Your First Number : ");
+            //MyVar1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please Enter Your Entry");
+            //MyVar2 = Console.Read();
+            //Console.WriteLine($"Hello {MyName}");
+            //Console.WriteLine($"You Enterd Number {MyVar1}");
+            //Console.WriteLine($"You Enterd Entry {MyVar2}");
+            //*************************************************
+            //5 - string MyName; char returnb;
+            //int MyVar1, MyVar2;        
+            //Console.WriteLine("Please Enter Your Name : ");
+            //MyName = Console.ReadLine();
+            //Console.WriteLine("Please Enter Your First Number : ");
+            //MyVar1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please Enter Your Entry");
+            //MyVar2 = Console.Read();
+            //returnb = Convert.ToChar(MyVar2);
+            //Console.WriteLine($"Hello {MyName}");
+            //Console.WriteLine($"You Enterd Number {MyVar1}");
+            //Console.WriteLine($"You Enterd Entry {MyVar2}");
+            //Console.WriteLine($"You Enterd Entry {returnb}");
+            //*********************************************
+            //HW - string FirstName, LastName;
+            //int SR;
+            //double TR;
+            //Console.WriteLine("Please Enter First Name :");
+            //FirstName = Console.ReadLine();
+            //Console.WriteLine("Please Enter Last Name :");
+            //LastName = Console.ReadLine();
+            //Console.WriteLine("Please Enter Your Salary :");
+            //SR = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please Enter Your TaxRate :");
+            //TR = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"Welcome {FirstName} {LastName}");
+            //Console.WriteLine($"Your Salary Per Month =  {SR}$");
+            //Console.WriteLine($"Your Salary For 12 Month  Without TR =  {SR * 12}$");
+            //Console.WriteLine($"Your Salary For 12 Month =  {SR * 12 - SR * 12 * 7/ 100}$");
+            //Console.WriteLine($"Your TaxRate For 12 Month =  {TR}");
+            //Console.WriteLine("Good Bye");
+        }
+    }
+}
