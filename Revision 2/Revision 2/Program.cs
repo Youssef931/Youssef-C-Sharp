52f6866f7b4cdfcc96f82468bce003355d941326
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-10-24 01:10:03 +0200

    Mathematical opreators
    Nazar<<Alwattar<<C#<<R2;

---
+++ Revision 2/Revision 2/Program.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Revision_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //25(1) - string myString = "This is my string variable";
+            //Console.WriteLine($"{myString}");
+            //*********************************************
+            //25(2) - string myString = "Nazar said\"This is args good cours\",and I think";
+            //Console.WriteLine($"{myString}");
+            //*********************************************
+            //25(3) - string myString = "Nazar said\'This is args good cours\',and I think";
+            //Console.WriteLine($"{myString}");
+            //*********************************************
+            //25(4) - string myString = "Nazar said\\This is args good cours\\,and I think";
+            //Console.WriteLine($"{myString}");
+            //*********************************************
+            //25(5) - string myString = "Nazar said This is args good cours, and I think\0";
+            //Console.WriteLine($"{myString}");
+            //*********************************************
+            //25(6) - string myString = "Nazar said This is args good cours, and I think\a";
+            //Console.WriteLine($"{myString}");
+            //*********************************************
+            //25(7) - string myString = "Nazar said This\b is args good cours, and I think";
+            //Console.WriteLine($"{myString}");
+            //*********************************************
+            //25(8) - string myString = "Nazar said This\n is args good cours, and I think";
+            //Console.WriteLine($"{myString}");
+            //*********************************************
+            //25(9) - string myString = "Nazar said This\r is args good cours, and I think";
+            //Console.WriteLine($"{myString}");
+            //*********************************************
+            //25(10) - string myString = "Nazar said This is args good cours, and I think";
+            //string myString2 = @"yes it is";
+            //string myString3 = myString + myString2;
+            //Console.WriteLine($"{myString3}");
+            //*********************************************
+            //25(Q1) - string myStr = @"Nazar said \This is a unique course\, and i think so ";
+            //Console.WriteLine($"{myStr}");
+            //*********************************************
+            //25(Q2,1) - string MyStr = "This is a\t good course";
+            //Console.WriteLine($"{MyStr}");
+            //*********************************************
+            //25(Q2,2) - string MyStr = "This is a \v good course";
+            //Console.WriteLine($"{MyStr}");
+            //*********************************************
+            //25(Q2,3) - string MyStr = "This is a \f good course";
+            //Console.WriteLine($"{MyStr}");
+            //*********************************************
+            //27,28(1) - int myVar1 = 15;
+            //int myvar2 = 10;
+            //int temp = myVar1 + myvar2;
+            //Console.WriteLine($"The result is {temp}");
+            //*********************************************
+            //27,28(2) - int myVar1 = 15;
+            //int myvar2 = 10;
+            //int temp = myVar1 + myvar2;
+            //int temp2 = myVar1 % myvar2;
+            //Console.WriteLine($"The result is {temp}, The result 2 is {temp2}");
+            //*********************************************
+            //27,28(3) - int myVar1 = 15;
+            //int myvar2 = 10;
+            //int temp = myVar1 + myvar2;
+            //int temp2 = myVar1 % myvar2;
+            //int temp3 = +myVar1;
+            //int temp4 = -myvar2;
+            ////Console.WriteLine($"The result is {temp}, The result 2 is {temp2}");
+            //Console.WriteLine($"{temp3}\t{temp4}");
+            //*********************************************
+            //27,28(4) - int myVar1 = 15;
+            //int myvar2 = 10;
+            //int temp = myVar1 + myvar2;
+            //int temp2 = myVar1 % myvar2;
+            //int myNegVar = -10;
+            //int temp3 = +myVar1;
+            //int temp4 = -myvar2;
+            //int temp5 = +myNegVar;
+            ////Console.WriteLine($"The result is {temp}, The result 2 is {temp2}");
+            //Console.WriteLine($"{temp3}\t{temp4}\t{temp5}");
+            //*********************************************
+            //27,28(5) - int myVar1 = 15;
+            //int myvar2 = 10;
+            //int temp = myVar1 + myvar2;
+            //int temp2 = myVar1 % myvar2;
+            //string myStr = "This is Wonderful";
+            //string myStr2 = "How is it ?, ";
+            //myStr = myStr2 + myStr;
+            //Console.WriteLine($"The result is {temp}, The result 2 is {temp2}");
+            //Console.WriteLine($"{myStr}");
+            //*********************************************
+            //27,28(Q1) - int myVar1 = 7;
+            //int myVar2 = 3;
+            //int temp1 = myVar1 + myVar2;
+            //int temp2 = myVar1 - myVar2;
+            //int temp3 = myVar1 / myVar2;
+            //int temp4 = myVar1 * myVar2;
+            //int temp5 = myVar1 & myVar2;
+            //Console.WriteLine($"{temp1}\n{temp2}\n{temp3}\n{temp4}\n{temp5}");
+            //*********************************************
+            //27,28(Q2) - string myStr1 = "Iam";
+            //string myStr2 = " Smart";
+            //myStr1 = myStr1 + myStr2;
+            //Console.WriteLine($"{myStr1}");
+        }
+    }
+}
