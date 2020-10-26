d5869d11d940b83d18b2c0cebc39ccb9d55f01cd
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-10-27 00:54:29 +0200

    Revision 3
    Nazar<<Alwattar<<C#<<R3;

---
+++ Revision 3/Revision 3/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Revision_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //29,30(1) - int myVar1 = 2;
+            //int temp1 = ++myVar1;
+            //Console.WriteLine($"{temp1}");
+            //*********************************************
+            //29,30(2) - int myVar1 = 2;
+            //int temp1 = myVar1++;
+            //Console.WriteLine($"{temp1}");
+            //*********************************************
+            //29,30(3) - int myVar1 = 2;
+            //int temp1 = --myVar1;
+            //Console.WriteLine($"{temp1}");
+            //*********************************************
+            //29,30(4) - int myVar1 = 2;
+            //int temp1 = myVar1--;
+            //Console.WriteLine($"{temp1}");
+            //*********************************************
+            //29,30(Q1) - int Var1 = 5;
+            //int temp1 = Var1++;
+            //Console.WriteLine($"{temp1}");
+            ////Q1 = 5;
+            //*********************************************
+            //29,30(Q2) - int Var1 = 5;
+            //int temp1 = ++Var1;
+            //Console.WriteLine($"{temp1}");
+            ////Q2 = 6;
+            //*********************************************
+            //29,30(Q3) - int temp, Var1 = 7, Var2 = 2;
+            //temp = Var1-- / ++Var2;
+            //Console.WriteLine($"{temp}");
+            ////Q3 = 2;
+            //*********************************************
+            //29,30(5) - int Var1 = 7, Var2 = 3;
+            //Var1 = Var2;
+            //Console.WriteLine($"{Var1}");
+            //*********************************************
+            //29,30(6) - int Var1 = 7, Var2 = 3;
+            //Var1 = Var2;
+            //Console.WriteLine($"{Var1}\t{Var2}");
+            //*********************************************
+            //29,30(7) - int Var1 = 7, Var2 = 3;
+            //Var2 = Var1;
+            //Console.WriteLine($"{Var1}\t{Var2}");
+            //*********************************************
+            //29,30(8) - int Var1 = 7, Var2 = 3;
+            //Var1 += Var2;
+            //Console.WriteLine($"{Var1}\t{Var2}");
+            //*********************************************
+            //29,30(9) - int Var1 = 7, Var2 = 3;
+            //Var2 += Var1;
+            //Console.WriteLine($"{Var1}\t{Var2}");
+            //*********************************************
+            //29,30(10) - int Var1 = 7, Var2 = 3;
+            //Var1 -= Var2;
+            //Console.WriteLine($"{Var1}\t{Var2}");
+            //*********************************************
+            //29,30(11) - int Var1 = 7, Var2 = 3;
+            //Var1 -= Var2;
+            //Var1 %= Var2;
+            //Console.WriteLine($"{Var1}\t{Var2}");
+            //*********************************************
+            //29,30(Q1) - int temp, Var1 = 7, Var2 = 2;
+            //Var1 /= ++Var2;
+            //Console.WriteLine($"{Var1}");
+            ////Q1 = 2;
+            //*********************************************
+            //32(1) - bool myRes;
+            //int Var1 = 5, Var2 = 8;
+            //myRes = Var1 == 6;
+            //Console.WriteLine($"{myRes}");
+            //*********************************************
+            //32(2) - bool myRes;
+            //int Var1 = 5, Var2 = 8;
+            //myRes = Var1 == 5;
+            //Console.WriteLine($"{myRes}");
+            //*********************************************
+            //32(3) - bool myRes;
+            //int Var1 = 5, Var2 = 8;
+            //myRes = Var1 != 5;
+            //Console.WriteLine($"{myRes}");
+            //*********************************************
+            //32(4) - bool myRes;
+            //int Var1 = 5, Var2 = 8;
+            //myRes = Var1 != 8;
+            //Console.WriteLine($"{myRes}");
+            //*********************************************
+            //32(5) - bool myRes;
+            //int Var1 = 5, Var2 = 8;
+            //myRes = Var1 != 8;
+            //myRes = (Var1 <= 5) && (Var2 > 8);
+            //Console.WriteLine($"{myRes}");
+            //*********************************************
+            //32(Q1,1) - bool MyRes;
+            //int Var1 = 9, Var2 = 11;
+            //MyRes = (Var1 < 10) && (Var2 <= 11);
+            //Console.WriteLine($"{MyRes}");
+            ////Q1(1) = T;
+            //*********************************************
+            //32(Q1,2) - bool MyRes;
+            //int Var1 = 9, Var2 = 11;
+            //MyRes = (Var1 < 10) || (Var2 <= 11);
+            //Console.WriteLine($"{MyRes}");
+            ////Q1(2) - T;
+        }
+    }
+}
