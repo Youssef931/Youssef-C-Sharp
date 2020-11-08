8e580579ae5b52758a58001e51044c3841cdc826
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-11-08 23:44:46 +0200

    Revision 4
    Nazar<<Alwattar<<C#<<R4;

--- Revision 4/Revision 4/Program.cs
+++ Revision 4/Revision 4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace Revision_4
 {
@@ -74,23 +75,107 @@ namespace Revision_4
             //    }
             //}
             //*********************************************
-            int Var = 4, Var2 = 6;
-            if (Var <= 3 && Var2 >= 5) 
-            {
-                Console.WriteLine("Choice1 is true");
-            }
-            else if (Var <= 4 && Var2 >=6)
-            {
-                Console.WriteLine("Choice2 is true");
-            }
-            else if (Var2 > 5)
-            {
-                Console.WriteLine("Choice3 is true");
-            }
-            else
-            {
-                Console.WriteLine("No One is True");
-            }
+            //int Var = 4, Var2 = 6;
+            //if (Var <= 3 && Var2 >= 5) 
+            //{
+            //    Console.WriteLine("Choice1 is true");
+            //}
+            //else if (Var <= 4 && Var2 >=6)
+            //{
+            //    Console.WriteLine("Choice2 is true");
+            //}
+            //else if (Var2 > 5)
+            //{
+            //    Console.WriteLine("Choice3 is true");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No One is True");
+            //}
+            //*********************************************
+            //36(1) - string myAnimal = "Cat";
+            //switch (myAnimal)
+            //{
+            //    case "Canary":
+            //        Console.WriteLine("Canary");
+            //        break;
+            //    case "Cat":
+            //        Console.WriteLine("Cat");
+            //        break;
+            //    case "Camel":
+            //        Console.WriteLine("Camel");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No one");
+            //        break;
+            //}
+            //*********************************************
+            //36(2) - int Var = 15;
+            //switch (Var)
+            //{
+            //    case 10:
+            //        Console.WriteLine("10");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("7");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("4");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No one");
+            //        break;
+            //}
+            //*********************************************
+            //36(3) - int Var = 15;
+            //switch (Var)
+            //{
+            //    case 10:
+            //        Console.WriteLine("10");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("7");
+            //        break;
+            //    case 15:
+            //        Console.WriteLine("15");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No one");
+            //        break;
+            //}
+            //*********************************************
+            //36(Q1) - string WhatHesay = "Hello";
+            //switch (WhatHesay)
+            //{
+            //    case "Hello":
+            //        Console.WriteLine("hello");
+            //        break;
+            //    case "Bye":
+            //        Console.WriteLine("bye");
+            //        break;
+            //    case "Welcome":
+            //        Console.WriteLine("Welcome");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No one");
+            //        break;
+            //}
+            //*********************************************
+            //37(1) - int myvar = 9;
+            //string myRes = (myvar < 10) ? "Less Than 10" : "Grater Than 10";
+            //Console.WriteLine($"{myRes}");
+            //*********************************************
+            //37(2) - int myvar = 9;
+            //string myRes = (myvar < 8) ? "Less Than 10" : "Grater Than 10";
+            //Console.WriteLine($"{myRes}");
+            //*********************************************
+            //37(3) - string myName = "Nazar", Result;
+            //Result = (myName.Equals("Nazar")) ? "Yes" : "No";
+            //Console.WriteLine($"{Result}");
+            //*********************************************
+            //37(4) - string myName = "nazar", result;
+            //result = (myName.Equals("Nazar")) ? "Yes" : "No";
+            //Console.WriteLine($"{result}");
         }
     }
 }
