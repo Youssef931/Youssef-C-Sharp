169b235135cab4dc2a8f1763c98e2b2562f6fee4
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-11-13 00:57:12 +0200

    Revision 6
    Nazar<<Alwattar<<C#<<r6;

---
+++ Revision 6/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+namespace Revision_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //43(1) - for (int i = 0; i <= 5; i++)
+            //{
+            //    Console.WriteLine("Hello Youssef");
+            //    if (i > 2)
+            //    {
+            //        break;
+            //    }
+            //    for (int j = 0; j <= 4; j++)
+            //    {
+            //        Console.WriteLine("\t Hello World\n");
+            //    }
+            //}
+            //*********************************************
+            //43(2) - for (int i = 0; i <= 5; i++)
+            //{
+            //    Console.WriteLine("Hello Youssef");
+            //    //if (i > 2)
+            //    //{
+            //      //  break;
+            //    //}
+            //    for (int j = 0; j <= 4; j++)
+            //    {
+            //        Console.WriteLine("\t Hello World\n");
+            //    }
+            //}
+            //*********************************************
+            //43(Q1) - for (int i = 0; i <= 10; i++)
+            //{
+            //    for (int j = 0; j < 10; j++)
+            //    {
+            //        if (j == 8)
+            //        {
+            //            break;
+            //        }
+            //        Console.WriteLine($"{i} * {j} = {i * j}");
+            //    }
+            //    Console.WriteLine("********************");
+            //}
+            //*********************************************
+            //44(1) - for (int i = 0; i <= 5; i++)
+            //{
+            //    Console.WriteLine("Hello Youssef");
+            //    if (i > 2)
+            //    {
+            //        continue;
+            //    }
+            //    for (int j = 0; j <= 4; j++)
+            //    {
+            //        Console.WriteLine($"\tHello Nazar");
+            //    }
+            //}
+            //*********************************************
+            //44(2) - for (int i = 0; i <= 5; i++)
+            //{
+
+            //    if (i > 2)
+            //    {
+            //        continue;
+            //    }
+            //    for (int j = 0; j <= 4; j++)
+            //    {
+            //        Console.WriteLine("Hello Youssef"); 
+            //    }
+            //    Console.WriteLine($"\tHello Nazar");
+            //}
+            //*********************************************
+            //44(Q1) - for (int i = 0; i <= 10; i++)
+            //{
+            //    for (int j = 0; j < 10; j++)
+            //    {
+            //        if (j == 7)
+            //        {
+            //            continue;
+            //        }
+            //        Console.WriteLine($"{i} * {j} = {i * j}");
+            //    }
+            //    Console.WriteLine("********************");
+            //}
+            //**********************************************
+            //45(1) - long Var = 1000;
+            //float Var2 = Var;
+            //Console.WriteLine($"{Var2}");
+            //char Var3 = 'G';
+            //int intVar = Var3;
+            //*********************************************
+            //45(2) - long Var = 1000;
+            //float Var2 = Var;
+            ////Console.WriteLine($"{Var2}");
+            //char Var3 = 'G';
+            //int intVar = Var3;
+            //Console.WriteLine($"{intVar}");
+            //*********************************************
+            //45(3) - long Var = 1000;
+            //float Var2 = Var;
+            //float floaVar = 12.5f;
+            //int intVar2 = floaVar;
+            ////Console.WriteLine($"{Var2}");
+            //char Var3 = 'G';
+            //int intVar = Var3;
+            //Console.WriteLine($"{intVar}");
+            //*********************************************
+            //46(1) - byte Sbyte;
+            //int Int = 7;
+            //Sbyte = (byte)Int;
+            //Console.WriteLine($"Var int = {Int}\n");
+            //Console.WriteLine($"Sbyte Var = {Sbyte}");
+            //*********************************************
+            //46(2) - byte Sbyte;
+            //int Int = 350;
+            //Sbyte = (byte)Int;
+            //Console.WriteLine($"Var int = {Int}\n");
+            //Console.WriteLine($"Sbyte Var = {Sbyte}");
+            //*********************************************
+            //46(3) - byte Sbyte;
+            //int Int = 350;
+            //Sbyte = (byte)Int;
+            //Console.WriteLine($"Var int = {Int}\n");
+            //Console.WriteLine($"Sbyte Var = {Sbyte}");
+            //*********************************************
+        }
+    }
+}
