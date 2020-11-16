1fcbff95fdf1573f8b78d358b43fd3acd63aec02
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-11-17 01:30:15 +0200

    Enums Variables and Challenge 20
    Nazar<<AL-wattar<<C#<<51;

---
+++ Enums Variables and Challenge 20/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Enums_Variables_and_Challenge_20
+{
+    class Program
+    {
+        //(1,2) - enum humanSex :sbyte
+        //{
+        //    male = 1,
+        //    female = 2
+        //}
+        //*************************************************
+        //(Q1) - enum Orentations :sbyte
+        //{
+        //    East = 2,
+        //    West  = 3,
+        //    Norht = 4,
+        //    South = 5
+        //}
+        static void Main(string[] args)
+        {
+            //(1) - sbyte humSbyte;
+            //string humGender;
+            //humanSex HumanSex = humanSex.male;
+            //Console.WriteLine($"Human Sex = {HumanSex}");
+            //*********************************************
+            //(2) - sbyte humSbyte;
+            //humanSex HumanSex = humanSex.male;
+            //Console.WriteLine($"Human Sex = {HumanSex}");
+            //humSbyte = (sbyte)HumanSex;
+            //Console.WriteLine($"Human Gender Index is = {humSbyte}");
+            //string humGender;
+            //humGender = Convert.ToString(HumanSex);
+            //Console.WriteLine($"Human Associates with index = {humGender}");
+            //*********************************************
+            //(Q1) - sbyte Orentations;
+            //string Orentations1;
+            //Orentations Orentations2 = Program.Orentations.East;
+            //Console.WriteLine($"First Orentation is {Orentations2}");
+            //Orentations = (sbyte)Orentations2;
+            //Console.WriteLine($"Orentation index is {Orentations}");
+            //Orentations1 = Convert.ToString(Orentations2);
+            //Console.WriteLine($"The Orentation Associates With index {Orentations1}\n\n");
+            //Orentations Orentations3 = Program.Orentations.West;
+            //Console.WriteLine($"Second Orentation is {Orentations3}");
+            //Orentations = (sbyte)Orentations3;
+            //Console.WriteLine($"Orentation index is {Orentations}");
+            //Orentations1 = Convert.ToString(Orentations3);
+            //Console.WriteLine($"The Orentation Associates With index {Orentations1}\n\n");
+            //Orentations Orentations4 = Program.Orentations.Norht;
+            //Console.WriteLine($"Third Orentation is {Orentations4}");
+            //Orentations = (sbyte)Orentations4;
+            //Console.WriteLine($"Orentation index is {Orentations}");
+            //Orentations1 = Convert.ToString(Orentations4);
+            //Console.WriteLine($"The Orentation Associates With index {Orentations1}\n\n");
+            //Orentations Orentations5 = Program.Orentations.South;
+            //Console.WriteLine($"Fourth Orentation is {Orentations5}");
+            //Orentations = (sbyte)Orentations5;
+            //Console.WriteLine($"Orentation index is {Orentations}");
+            //Orentations1 = Convert.ToString(Orentations3);
+            //Console.WriteLine($"The Orentation Associates With index {Orentations1}");
+        }
+    }
+}
