d2649649e5690f58fafdd08c8bdb81de556b555a
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-11-21 08:50:21 +0200

    Structure Type and Challenge 21
    Nazar<<Al-Wattar<<C#<<52;

---
+++ Structure Type and Challenge 21/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Structure_Type_and_Challenge_21
+{
+    class Program
+    {
+        //(1) - enum Toyota : byte
+        //{
+        //    RAV4 = 1,
+        //    Camery = 2,
+        //    Tundary = 3,
+        //    highland = 4
+        //}
+        //struct CarType
+        //{
+         // public Toyota ModelName;
+        //    public double ModelYear;
+        //}
+        //*************************************************
+        //(Q1) - enum Oreantations : byte
+        //{
+        //    east = 1,
+        //    west = 2,
+        //    north = 3,
+        //    south = 4
+        //}
+        //struct directions
+        //{
+        //    public Oreantations oreantations;
+        //    public double distant;
+        //}
+        static void Main(string[] args)
+        {
+            //(1) - CarType cartype;
+            //int cartype1 = -1;
+            //double ModelYear;
+            //Console.WriteLine($"1) RAV4\n2) Camery\n3) Tundra\n4) HighLand");
+            //do
+            //{
+            //    Console.WriteLine("Enter your car type");
+            //    cartype1 = Convert.ToInt32(Console.ReadLine());
+            //} while ((cartype1 < 1) || (cartype1 > 4));
+            //Console.WriteLine("Input a modelYear : ");
+            //ModelYear = Convert.ToDouble(Console.ReadLine());
+            //cartype.ModelName = (Toyota)cartype1;                                   
+            //cartype.ModelYear = ModelYear;
+            //Console.WriteLine($"CarType is {cartype.ModelName}And ModelYear is {cartype.ModelYear}");
+            //*********************************************
+            //(Q1) - directions oreantations1;
+            //int orentations2 = 1;
+            //double Distant;
+            //Console.WriteLine("1)East\n2)West\n3)North\n4)South");
+            //do
+            //{
+            //    Console.WriteLine("Enter your orentation");
+            //    orentations2 = Convert.ToInt32(Console.ReadLine());
+            //} while ((orentations2 < 1) || (orentations2 > 4));
+            //Console.WriteLine("Input The distance");
+            //Distant = Convert.ToDouble(Console.ReadLine());
+            //oreantations1.oreantations = (Oreantations)orentations2;
+            //oreantations1.distant = Distant;
+            //Console.WriteLine($"Orentations is {oreantations1.oreantations}, And Distant is {oreantations1.distant}");
+        }
+    }
+}
