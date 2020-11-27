ce9603c7f8393df78dfda6ab22ede39437cb4932
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-11-27 23:41:54 +0200

    Array and Foreach and Challenge 23
    Nazar<<Al-wattatr<<C#<<54;

---
+++ Array and Foreach and Challenge 23/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Array_and_Foreach_and_Challenge_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1) - const int myArray = 5;
+            //int[] MyNumber = new int[myArray] { 1, 5, 8, 9, 7 };
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine($"My Number {i + 1} = {MyNumber[i]}");
+            //}
+            //*********************************************
+            //(2) - const int myArray = 5;
+            //int[] MyNumber = new int[myArray] { 1, 5, 8, 9, 7 };
+            //for (int i = 0; i <= 5; i++)
+            //{
+            //    Console.WriteLine($"My Number {i + 1} = {MyNumber[i]}");
+            //}
+            //*********************************************
+            //(3) - const int myArray = 5;
+            //int[] MyNumber = new int[myArray] { 1, 5, 8, 9, 7 };
+            //foreach (int  item in MyNumber)
+            //{
+            //    Console.WriteLine($"My Number is {item}");
+            //}
+            //*********************************************
+            //(Q1) - const int Array = 15;
+            //int[] MyNum = new int[Array] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            //foreach (int item in MyNum)
+            //{
+            //    if (item == MyNum[14])
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine("My Number is " + $"{item}");
+            //}
+        }
+    }
+}
