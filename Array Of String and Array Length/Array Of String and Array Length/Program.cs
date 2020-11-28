1fb043789f0d6f7c2696d5a82c9aaf22225b4835
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-11-28 08:06:30 +0200

    Array Of String and Array Length
    Nazar<<Al-Wattar<<C#<<55;

---
+++ Array Of String and Array Length/Array Of String and Array Length/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Array_Of_String_and_Array_Length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1) - string[] MFN = { "Youssef", "Aly", "Sabry" };
+            //int MAL = MFN.Length;
+            //for (int i = 0; i < MAL; i++)
+            //{
+            //    Console.WriteLine($"{MFN[i]}");
+            //}
+            //Console.WriteLine($"MAL is {MAL}");
+            //*********************************************
+            //(2) - string[] MFN = { "Youssef", "Aly", "Sabry" };
+            //int MAL = MFN.Length;
+            //for (int i = 0; i < MAL; i++)
+            //{
+            //    Console.Write($"{MFN[i]} ");
+            //}
+            //Console.WriteLine($"MAL is {MAL}");
+            //*********************************************
+            //(3) - string[] MFN = { "Youssef", "Aly", "Sabry" };
+            //int MAL = MFN.Length;
+            //for (int i = 0; i < MAL; i++)
+            //{
+            //    Console.Write($"{MFN[i]} ");
+            //}
+            //Console.WriteLine($"\nMAL is {MAL}");
+            //*********************************************
+            //Revision on Lesson 12
+            //(1) - Console.WriteLine("Hello World");
+            //*********************************************
+            //(2) - Console.WriteLine("Hello World\nHello Live");
+            //Console.ReadKey();
+        }
+    }
+}
