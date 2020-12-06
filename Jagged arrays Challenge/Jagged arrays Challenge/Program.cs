1fb8b2a1a764b518d4a28b2fd3c45073e2eca51c
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-12-06 07:27:22 +0200

    Jagged Arrays Challeng 24
    Nazar<<AL-Wattar<<C#<<58;

---
+++ Jagged arrays Challenge/Jagged arrays Challenge/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Jagged_arrays_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1) - int[][] arr = new int[2][];
+            //arr[0] = new int[5] { 1, 3, 5, 7, 9 };
+            //arr[1] = new int[4] { 2, 4, 6, 8 };
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write($"Element({i}) ");
+            //    for (int j = 0; j < arr[i].Length; j++)
+            //    {
+            //        Console.Write($"{arr[i][j]} ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //*********************************************
+            //(Q1) - int[][]arr = new int[3][];
+            //arr[0] = new int[7] { 1, 5, 8, 9, 6, 4, 9 };
+            //arr[1] = new int[3] { 0, 4, 3 };
+            //arr[2] = new int[4] { 2, 9, 3, 10 };
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write($"Element({i}) ");
+            //    for (int j = 0; j < arr[i].Length; j++)
+            //    {
+            //        Console.Write($"{arr[i][j]} ");
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
