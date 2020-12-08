23a657dc425da74b1ceb75dd9476b91e1aea666a
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-12-08 06:59:55 +0200

    String Manipulation Operations and Challenge 25
    Nazar<<Al-Wattar<<C#<<59;

---
+++ String Manipulation Operations and Challenge 25/String Manipulation Operations and Challenge 25/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace String_Manipulation_Operations_and_Challenge_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1) - string MS = "Hello My Freinds";
+            //char[] MCA = MS.ToCharArray();
+            //foreach (char MC in MCA)
+            //{
+            //    Console.Write($"{MC},");
+            //}
+            //*********************************************
+            //(2) - string MS = "Hello My Freinds";
+            //char[] MCA = MS.ToCharArray();
+            //Console.WriteLine($"THe STR L is {MCA.Length}");
+            //foreach (char MC in MCA)
+            //{
+            //    Console.Write($"{MC},");
+            //}
+            //*********************************************
+            //(Q1) - string MS = "I am smart";
+            //char[] MCA = MS.ToCharArray();
+            //MCA[0] = 'H';
+            //MCA[1] = 'e';
+            //MCA[2] = '\'';
+            //MCA[3] = 's';
+            //foreach (char item in MCA)
+            //{
+            //    Console.Write($"{item}");
+            //}
+        }
+    }
+}
