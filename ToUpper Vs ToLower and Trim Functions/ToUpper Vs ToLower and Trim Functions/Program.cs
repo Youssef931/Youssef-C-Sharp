ebb11b77d65c6dd0c903334ec0e0108a06d3afbc
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-12-08 07:30:28 +0200

    ToUpper Vs ToLower & Trim Functions
    Nazar<<AL-Wattar<<C#<<60;

---
+++ ToUpper Vs ToLower and Trim Functions/ToUpper Vs ToLower and Trim Functions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ToUpper_Vs_ToLower_and_Trim_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1) - string MS, MS2;
+            //Console.WriteLine("Please Enter Your String");
+            //MS = Console.ReadLine();
+            //MS2 = MS.ToUpper();
+            //Console.WriteLine($"{MS2}");
+            //*********************************************
+            //(2) - string MS, MS2;
+            //Console.WriteLine("Please Enter Your String");
+            //MS = Console.ReadLine();
+            //MS2 = MS.ToUpper();
+            //Console.WriteLine($"{MS2}");
+            //MS2 = MS.ToLower();
+            //Console.WriteLine($"{MS2}");
+            //*********************************************
+            //(3) - string MS, MS2;
+            //Console.WriteLine("Please Enter Your String");
+            //MS = Console.ReadLine();
+            ////MS2 = MS.ToUpper();
+            ////Console.WriteLine($"{MS2}");
+            ////MS2 = MS.ToLower();
+            ////Console.WriteLine($"{MS2}");
+            //MS2 = MS.Trim();
+            //Console.WriteLine($"{MS2}");
+            //*********************************************
+            string MS, MS2;
+            char[] MTC = { 'h', 'e', 'i', ' ' };
+            Console.WriteLine("PLease Enter Your String");
+            MS = Console.ReadLine();
+            MS = MS.ToLower();
+            MS2 = MS.Trim(MTC);
+            Console.WriteLine($"{MS2}");
+        }
+    }
+}
