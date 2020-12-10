ef446f1d83fd8e7c9fcb6570b39223aeefb759a3
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-12-10 06:49:49 +0200

    Trim Function2 and Pads Functions and Challenge 25
    Nazar<<Al-Wattar<<C#<<61;

---
+++ Trim Function2 and Pads Functions and Challenge 25/Trim Function2 and Pads Functions and Challenge 25/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Trim_Function2_and_Pads_Functions_and_Challenge_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1) - string MS, MS2;
+            //char[] MTC = { 'h', 'e', 'i', ' ' };
+            //Console.WriteLine("Please Enter Your String");
+            //MS = Console.ReadLine();
+            //MS = MS.ToLower();
+            //MS2 = MS.TrimStart(MTC);
+            //Console.WriteLine($"{MS2}");
+            //MS2 = MS.TrimEnd(MTC);
+            //Console.WriteLine($"{MS2}");
+            //*********************************************
+            //(2) - string MS, MS2;
+            //char[] MTC = { 'h', 'e', 'i', ' ' };
+            //Console.WriteLine("Please Enter Your String");
+            //MS = Console.ReadLine();
+            //MS = MS.ToLower();
+            //MS2 = MS.TrimStart(MTC);
+            //Console.WriteLine($"{MS2}");
+            //MS2 = MS.TrimEnd(MTC);
+            //Console.WriteLine($"{MS2}");
+            //MS2 = MS.PadLeft(25, '-');
+            //Console.WriteLine($"{MS2}");
+            //MS2 = MS.PadRight(25, '-');
+            //Console.WriteLine($"{MS2}");
+            //*********************************************
+            //(3) - string MS, MS2;
+            //char[] MTC = { 'h', 'e', 'i', ' ' };
+            //Console.WriteLine("Please Enter Your String");
+            //MS = Console.ReadLine();
+            //MS = MS.ToLower();
+            //MS2 = MS.TrimStart(MTC);
+            //Console.WriteLine($"{MS2}");
+            //MS2 = MS.TrimEnd(MTC);
+            //Console.WriteLine($"{MS2}");
+            //MS2 = MS.PadLeft(25, '-');
+            //Console.WriteLine($"{MS2}");
+            //MS2 = MS.PadRight(25, '-');
+            //Console.WriteLine($"{MS2}");
+            //*********************************************
+            //(4) - string MS, MS2;
+            //char[] MTC = { 'h', 'e', 'i', ' ' };
+            //Console.WriteLine("Please Enter Your String");
+            //MS = Console.ReadLine();
+            //MS = MS.ToLower();
+            //MS2 = MS.TrimStart(MTC);
+            //Console.WriteLine($"{MS2}");
+            //MS2 = MS.TrimEnd(MTC);
+            //Console.WriteLine($"{MS2}");
+            //MS2 = MS.PadLeft(25, ' ');
+            //Console.WriteLine($"{MS2}");
+            //MS2 = MS.PadRight(25, 'k');
+            //Console.WriteLine($"{MS2}");
+            //*********************************************
+            //(5) - string MS, MS2;
+            //char[] MTC = { 'h', 'e', 'i', ' ' };
+            //Console.WriteLine("Please Enter Your String");
+            //MS = Console.ReadLine();
+            //MS = MS.ToLower();
+            //MS2 = MS.TrimStart(MTC);
+            //Console.WriteLine($"{MS2}");
+            //MS2 = MS.TrimEnd(MTC);
+            //Console.WriteLine($"{MS2}");
+            //MS2 = MS.PadLeft(25, ' ');
+            //Console.WriteLine($"{MS2}");
+            //MS2 = MS.PadRight(25, 'k');
+            //Console.WriteLine($"{MS2}");
+            //*********************************************
+            //(Q1) - string MS, MS2;
+            //Console.WriteLine("Please Enter Your String");
+            //MS = Console.ReadLine();
+            //MS2 = MS.PadRight(14, 'e').PadRight(15, 's').PadRight(16, 't');
+            //Console.WriteLine($"{MS2}");
+        }
+    }
+}
