194a148c430342d5233542aced86bf17734f258c
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-12-10 07:08:10 +0200

    Split Function with String Type
    Nazar<<Al-Wattar<<C#<<62;

---
+++ Split Function with String Type/Split Function with String Type/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Split_Function_with_String_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1) - string MS;
+            //char[] S = { ' '};
+            //MS = Console.ReadLine();
+            //string[] MNW;
+            //MNW = MS.Split(S);
+            //foreach (string W in MNW)
+            //{
+            //    Console.WriteLine($"{W}");
+            //}
+            //*********************************************
+            //(2) - string MS;
+            //char[] S = { 'u' };
+            //MS = Console.ReadLine();
+            //string[] MNW;
+            //MNW = MS.Split(S);
+            //foreach (string W in MNW)
+            //{
+            //    Console.WriteLine($"{W}");
+            //}
+            //*********************************************
+            //(3) - string MS;
+            //char[] S = { ' ','u' };
+            //MS = Console.ReadLine();
+            //string[] MNW;
+            //MNW = MS.Split(S);
+            //foreach (string W in MNW)
+            //{
+            //    Console.WriteLine($"{W}");
+            //}
+        }
+    }
+}
