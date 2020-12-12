b00f34bde580520243a0107498811248db6bbdeb
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-12-12 06:30:35 +0200

    First Function
    Nazar<<Al-Wattar<<C#<<63;

---
+++ 63 - First Function/63 - First Function/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _63___First_Function
+{
+    class Program
+    {
+        //(1) - static void P_N()
+        //{
+        //    Console.Write("My Name Is Mohammed");
+        //}
+        static void Main(string[] args)
+        {
+            //(1) - P_N();
+        }
+    }
+}
