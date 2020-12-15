2bf8fdf1f7c123d1308486d48b151dbd3bb1ba6b
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-12-15 07:07:49 +0200

    Variable Scope
    Nazar<<Al-Wattar<<C#<<68;

---
+++ Variables Scope/Variables Scope/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Variables_Scope
+{
+    class Program
+    {
+        //(1) - static void PMS()
+        //{
+        //    string MS = "My Str Inside The PMS";
+        //    Console.WriteLine($"{MS}");
+        //}
+        //*************************************************
+        //(2) - static string MS;
+        //static void PMS()
+        //{
+        //    string MS = "My Str Inside The PMS";
+        //    Console.WriteLine($"{MS}");
+        //    Console.WriteLine($"{Program.MS}");
+        //}
+        //*************************************************
+        static string MS2;
+        static void PMS()
+        {
+            string MS = "My Str Inside The PMS";
+            Console.WriteLine($"{MS}");
+            Console.WriteLine($"{MS2}");
+        }
+        static void Main(string[] args)
+        {
+            //(1) - string MS = "My Str Inside The Main";
+            //PMS();
+            //Console.WriteLine($"{MS}");
+            //*********************************************
+            //(2) - string MS = "My Str Inside The Main";
+            //Program.MS = "Global Var";
+            //PMS();
+            //Console.WriteLine($"{MS}");
+            //Console.WriteLine($"{Program.MS}");
+            //*********************************************
+            //(3) - string MS = "My Str Inside The Main";
+            //MS2 = "Global Var";
+            //PMS();
+            //Console.WriteLine($"{MS}");
+            //Console.WriteLine($"{MS2}");
+        }
+    }
+}
