543bc76313af772adeab30fc22b45ebfa7b12af8
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-12-15 07:23:35 +0200

    Variable Scope In Structure
    Nazar<<Alwattar<<C#<<69;

---
+++ Variable Scope in Structures/Variable Scope in Structures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Variable_Scope_in_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, index = 0;
+            for (i = 0; i < 10; i++)
+            {
+                index = i;
+                Console.WriteLine($"{index}");
+            }
+            Console.WriteLine($"Last Index OutPut int loop : {index}");
+        }
+    }
+}
