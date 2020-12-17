2ec1cd495276e6a7d127621747c35810d2173166
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-12-17 07:25:58 +0200

    Overloading and Challenge 29
    Nazar<<Al-Wattar<<C#<<72;

---
+++ Overloading and Challenge 29/Overloading and Challenge 29/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Overloading_and_Challenge_29
+{
+    class Program
+    {
+        //(1) - public static int PN(int MN)
+        //{
+        //    return MN;
+        //}
+        //*************************************************
+        //(2) - public static int PN(int MN)
+        //{
+        //    return MN;
+        //}
+        //public static double PN(double MN)
+        //{
+        //    return MN;
+        //}
+        //*************************************************
+        //(Q1) - public static int A(int[] MN)
+        //{
+        //    int MNV = MN[0];
+        //    for (int i = 1; i < MN.Length; i++)
+        //    {
+        //        if (MN[i]<MNV)
+        //        {
+        //            MNV = MN[i];
+        //        }
+        //    }
+        //    return MNV;
+        //}
+        //public static double A(double[] MN)
+        //{
+        //    int MNV = (int)MN[0];
+        //    for (int i = 1; i < MN.Length; i++)
+        //    {
+        //        if (MN[i]<MNV)
+        //        {
+        //            MNV = (int)MN[i];
+        //        }
+        //    }
+        //    return MNV;
+        //}
+        static void Main(string[] args)
+        {
+            //(1) - int MN = 9;
+            //Console.Write(PN(MN));
+            //*********************************************          
+            //(2) - int MN = 9;
+            //Console.WriteLine(PN(MN));
+            //*********************************************
+            //(Q1) - int[] MN2 = { 8, 10, 90, 100, 5, 500, 70, 689, 120, 890 };
+            //double[] MN3 = { 8, 10, 90, 100, 5, 500, 70, 689, 120, 890 };
+            //A(MN2);
+            //A(MN3);
+            //Console.WriteLine($"{A(MN2)}");
+            //Console.WriteLine($"{A(MN3)}");
+        }
+    }
+}
