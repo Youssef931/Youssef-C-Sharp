dbc246dd14eb4df150750dcea2475ce9de6486d6
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-12-19 07:21:05 +0200

    Delegate Type
    Nazar<<Al-Wattar<<C#<<73;

---
+++ Delegate Type/Delegate Type/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Delegate_Type
+{
+    delegate int MNN(int MN);
+    class Program
+    {
+        static int MXN = 3;
+        static int AF (int N)
+        {
+            MXN += N;
+            return MXN;
+        }
+        static int MF (int N)
+        {
+            MXN *= N;
+            return MXN;
+        }
+        static void Main(string[] args)
+        {
+            MNN N1 = new MNN(AF);
+            MNN N2 = new MNN(MF);
+            N1(4);
+            Console.WriteLine($"{MXN}");
+            N2(5);
+            Console.WriteLine($"{MXN}");
+        }
+    }
+}
