897513d5aecd0cd331757dc4783c8dda70f734d9
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-12-19 07:58:09 +0200

    Delegate Type
    Nazar<<Al-Wattar<,C#<<73<<R22,23;

--- Delegate Type/Delegate Type/Program.cs
+++ Delegate Type/Delegate Type/Program.cs
@@ -2,28 +2,65 @@
 
 namespace Delegate_Type
 {
-    delegate int MNN(int MN);
+    //(1) - delegate int MNN(int MN);
     class Program
     {
-        static int MXN = 3;
-        static int AF (int N)
-        {
-            MXN += N;
-            return MXN;
-        }
-        static int MF (int N)
-        {
-            MXN *= N;
-            return MXN;
-        }
+        //(1) - static int MXN = 3;
+        //static int AF (int N)
+        //{
+        //    MXN += N;
+        //    return MXN;
+        //}
+        //static int MF (int N)
+        //{
+        //    MXN *= N;
+        //    return MXN;
+        //}
         static void Main(string[] args)
         {
-            MNN N1 = new MNN(AF);
-            MNN N2 = new MNN(MF);
-            N1(4);
-            Console.WriteLine($"{MXN}");
-            N2(5);
-            Console.WriteLine($"{MXN}");
+            //(1) - MNN N1 = new MNN(AF);
+            //MNN N2 = new MNN(MF);
+            //N1(4);
+            //Console.WriteLine($"{MXN}");
+            //N2(5);
+            //Console.WriteLine($"{MXN}");
+            //***************R On L(22,23)*****************
+            //(1) - int CMN = Char.MinValue;
+            //int CMX = Char.MaxValue;
+            //char MV = 'G';
+            //Console.WriteLine($"MNC = {CMN}, MXC = {CMX}, MV = {MV}");
+            //*********************************************
+            //(2) - int CMN = Char.MinValue;
+            //int CMX = Char.MaxValue;
+            //char MV = 'G';
+            //int MVV = 'G';
+            //Console.WriteLine($"MNC = {CMN}, MXC = {CMX}, MV = {MV}, MVV = {MVV}");
+            //*********************************************
+            //(3) - int CMN = Char.MinValue;
+            //int CMX = Char.MaxValue;
+            //char MV = 'G';
+            //int MVV = 'G';
+            //Console.WriteLine($"MNC = {CMN}, MXC = {CMX}, MV = {MV}, MVV = {MVV}");
+            //string MS = "This Is My Variable";
+            //Console.WriteLine($"\n My String is {MS}");
+            //*********************************************
+            //(4) - int CMN = Char.MinValue;
+            //int CMX = Char.MaxValue;
+            //char MV = 'G';
+            //int MVV = 'G';
+            //Console.WriteLine($"MNC = {CMN}, MXC = {CMX}, MV = {MV}, MVV = {MVV}");
+            //string MS = "This Is My Variable";
+            //Console.WriteLine($"\nMy String is {MS}");
+            //bool MB = true;
+            //Console.WriteLine($"My Boolean V = {MB}");
+            //*********************************************
+            //(Q1) - int MC1 = 'A';
+            //int MC2 = 'a';
+            //string MS1 = $"This is My A Value {MC1}";
+            //string MS2 = $"This is My a Value {MC2}";
+            //Console.WriteLine(MS1);
+            //Console.WriteLine(MS2);
+            //Console.WriteLine($"{MS1} And {MS2}");
         }
     }
 }
