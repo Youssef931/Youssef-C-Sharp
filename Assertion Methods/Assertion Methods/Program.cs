075fa9085929a6cc85779450bcbb8ad1dda98821
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2020-12-22 06:23:56 +0200

    Assertion Method
    Nazar<<Al-Wattar<<C#<<76;

---
+++ Assertion Methods/Assertion Methods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+using System.Diagnostics;
+namespace Assertion_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1) - WriteLine($"Enter A Value");
+            //int i = Convert.ToInt32(ReadLine());
+            //if (i >= 100)
+            //{
+            //    WriteLine($"{i}");
+            //}
+            //else
+            //{
+            //    Debug.WriteLine("We Are Here");
+            //    Debug.Assert(i >= 100, "Your Money Are Not Enough", "You Must Enter A value Greater Than or Equal 100$");
+                
+            //}
+            //WriteLine("Done");
+            //ReadKey();
+            //*********************************************
+            //(2) - WriteLine($"Enter A Value");
+            //int i = Convert.ToInt32(ReadLine());
+            //if (i >= 100)
+            //{
+            //    WriteLine($"{i}");
+            //}
+            //else
+            //{
+            //    Debug.WriteLine("We Are Here");
+            //    //Debug.Assert(i >= 100, "Your Money Are Not Enough", "You Must Enter A value Greater Than or Equal 100$");
+            //    Trace.Assert(i >= 100, "Your Money Are Not Enough", "You Must Enter A value Greater Than or Equal 100$");
+            //}
+            //WriteLine("Done");
+            //ReadKey();
+        }
+    }
+}
