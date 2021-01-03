7111a6e57b1cad5ae23b0b2682fd5cadd5a6de9b
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-01-03 23:57:53 +0200

    Multicasting Delegate
    Nazar<<Al-wattar<<C#<<82;

---
+++ Multicasting Delegate/Multicasting Delegate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Multicasting_Delegate
+{ 
+    delegate int MNN(int MN);
+    class Program
+    {
+        static int MXN = 3;
+        static int AF(int N)
+        {
+            MXN += N;
+            return MXN;
+        }
+        static int MF(int N)
+        {
+            MXN *= N;
+            return MXN;
+        }
+        static void Main(string[] args)
+        {
+            MNN n;
+            MNN n1 = new MNN(AF);
+            MNN n2 = new MNN(MF);
+            n1(4);
+            Console.WriteLine(MXN);
+            n2(5);
+            Console.WriteLine(MXN);
+            n = n1 + n2;                                                                                                                     
+            n(5);
+            Console.WriteLine(MXN);
+        }
+    }
+}
