d8ae35307f66a5f57f568d86967921d298727cc1
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-01-06 00:40:21 +0200

    83 - Using Namespaces in Practical
    Nazar<<Al-Wattar<<C#<<83;

---
+++ 83 - Using Namespaces in Practical/83 - Using Namespaces in Practical/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+using OB = B.OB.MB;
+using YB = B.YB.MB;
+namespace _83___Using_Namespaces_in_Practical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Those Are My Family");
+            YB.print();
+            OB.print();
+            ReadKey();
+        }
+    }
+}
+namespace B
+{
+    namespace YB
+    {
+        class MB
+        {
+            public static void print()
+            {
+                WriteLine("Yassen");
+            }
+        }
+    }
+    namespace OB
+    {
+        class MB
+        {
+            public static void print()
+            {
+                WriteLine("Nada");
+            }
+        }
+    }
+}
