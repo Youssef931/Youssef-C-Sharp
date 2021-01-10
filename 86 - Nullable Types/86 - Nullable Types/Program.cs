0e22b1d6bd695d67313084f45ac5e2b9b961e99c
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-01-10 23:33:34 +0200

    86 - Nullable Types
    Nazar<<Al-Wattar<<C#<<86;

---
+++ 86 - Nullable Types/86 - Nullable Types/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+namespace _86___Nullable_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1) - string MV = null;
+            //WriteLine(MV);
+            //ReadKey();
+            //******************************************
+            //(2) - string MV = "null";
+            //WriteLine(MV);
+            //ReadKey();
+            //******************************************
+            //(3) - string MV = null;
+            //int? MN = null;
+            //WriteLine(MV);
+            //WriteLine(MN);
+            //ReadKey();
+        }
+    }
+}
