990b82d34d4e397ca211949c177077e88f4783f5
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-01-11 07:22:46 +0200

    88 - Params Keyword and Challenge 31
    Nazar<<AAl-Wattar<<C#<<88;

--- 88 - Params Keyword and Challenge 31/88 - Params Keyword and Challenge 31/Program.cs
+++ 88 - Params Keyword and Challenge 31/88 - Params Keyword and Challenge 31/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
-
+using static System.Console;
 namespace _88___Params_Keyword_and_Challenge_31
 {
     class Program
     {
+        //(1) - static double CA(params double[] values)
+        //{
+        //    WriteLine($"You sent me {values.Length} doubles");
+        //    double S = 0;
+        //    if (values.Length == 0)
+        //        return S;
+        //    for (int i = 0; i < values.Length; i++)
+        //        S += values[i];
+        //        return (S / values.Length);
+        //**********************************************
+        //(Q1) - static int MX(params int[] V)
+        //{
+        //    int MX1 = 0; ;
+        //    for (int i = 0; i < V.Length; i++)
+        //    {
+        //        if (MX1<V[i])
+        //        {
+        //            MX1 = V[i];
+        //        }
+        //    }
+        //    return MX1;
+        //}
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //(1) - double A;
+            //A = CA(4.0, 3.2, 5.7, 64, 87.2);
+            //WriteLine($"Average of data is: {A}");
+            //double[] D = { 4.0, 3.2, 5.7 };
+            //A = CA(D);
+            //WriteLine($"Average of Data is: {A}");
+            //WriteLine();
+            //WriteLine($"Average of Data is: {CA()}");
+            //******************************************
+            //(Q1) - int[] A = { 3, 76, 89, 54, 36, 12 };
+            //WriteLine(MX(A));
         }
     }
 }
