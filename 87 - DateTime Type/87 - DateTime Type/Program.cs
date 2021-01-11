9fad166f30e021db30a176ab2d4ccc5b81740d59
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-01-11 06:58:23 +0200

    87 - DateTime Type
    Nazar<<Al-Wattar<<C#<<87;

---
+++ 87 - DateTime Type/87 - DateTime Type/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using static System.Console;
+namespace _87___DateTime_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1) - WriteLine(DateTime.Now.ToString("hh:mm:ss\nMM dd yyyy"));
+            //******************************************
+            //(2) - DateTime T = DateTime.Now;
+            //string F = "MM dd yyyy\nhh:mm";
+            //WriteLine(T.ToString(F));
+            //******************************************
+            //(3) - DateTime T = DateTime.Now;
+            //string F = "MM dd d HH:mm yyyy";
+            //WriteLine(T.ToString(F));
+            //******************************************
+            //(4) - DateTime now = DateTime.Now;
+            //WriteLine(now.ToString("d"));
+            //WriteLine(now.ToString("D"));
+            //WriteLine(now.ToString("f"));
+            //WriteLine(now.ToString("g"));
+            //WriteLine(now.ToString("G"));
+            //WriteLine(now.ToString("m"));
+            //WriteLine(now.ToString("M"));
+            //WriteLine(now.ToString("o"));
+            //WriteLine(now.ToString("O"));
+            //WriteLine(now.ToString("s"));
+            //WriteLine(now.ToString("t"));
+            //WriteLine(now.ToString("T"));
+            //WriteLine(now.ToString("u"));
+            //WriteLine(now.ToString("U"));
+            //WriteLine(now.ToString("y"));
+            //WriteLine(now.ToString("Y"));
+            //******************************************
+            //(5) - DateTime N = DateTime.Now;
+            //string R = N.ToString("s ");
+            //WriteLine($"{N} [s] = {R}");
+            //******************************************
+            DateTime N = DateTime.Today;
+            for (int i = 0; i < 7; i++)
+            {
+                WriteLine(N.ToString("dddd"));
+                N = N.AddDays(1);
+            }
+        }
+    }
+}
---
+++ 88 - Params Keyword and Challenge 31/88 - Params Keyword and Challenge 31/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _88___Params_Keyword_and_Challenge_31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
