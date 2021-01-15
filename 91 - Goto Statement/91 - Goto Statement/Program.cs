c04da68f73f64a67d804587b3568ecba48eb7da4
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-01-15 23:57:23 +0200

    91 - Goto Statement
    Nazar<<Al-Wattar<<C#<<91;

---
+++ 91 - Goto Statement/91 - Goto Statement/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+namespace _91___Goto_Statement
+{
+    class Program
+    {
+        static int MF()
+        {
+            int C = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (j == 5)
+                    {
+                        goto ML;
+                    }
+                    C++;
+                }
+                ML:
+                continue;
+            }
+            return C;
+        }
+        static void Main(string[] args)
+        {
+            int C = 0;
+            //ML2:
+            if (C<10)
+            {
+                C++;
+                //goto ML2;
+            }
+            else
+            {
+                WriteLine("Done");
+                WriteLine(C);
+            }
+        }
+    }
+}
