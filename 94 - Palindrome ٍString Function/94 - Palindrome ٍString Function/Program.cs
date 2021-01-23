d4dda4149817ddbff6344e7da462d5514a035215
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-01-23 23:54:41 +0200

    94 - Palindrome ٍString Function
    Nazar<<Al-Wattar<<C#<<94;

---
+++ 94 - Palindrome ٍString Function/94 - Palindrome ٍString Function/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+namespace _94___Palindrome_ٍString_Function
+{
+    class Program
+    {
+        public static bool MPF(string V)
+        {
+            int MN = 0;
+            int MX = V.Length - 1;
+            while (true)
+            {
+                if (MN > MX)
+                {
+                    return true;
+                }
+                char A = V[MN];
+                char B = V[MX];
+                if (char.ToLower(A) != char.ToLower(B))
+                {
+                    return false;
+                }
+                MN++;
+                MX--;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] MA = { "civic", "deified", "deleved", "Perls", "is", "not", "A", "palindroom", "" };
+            foreach (string item in MA)
+            {
+                Console.WriteLine($"{item} = {MPF(item)}");
+            }
+        }
+    }
+}
