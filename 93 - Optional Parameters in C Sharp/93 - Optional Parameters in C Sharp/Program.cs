a6f74385202157bef4ed34a91ef17092381271e6
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-01-23 23:30:51 +0200

    93 - Optional Parameters in C Sharp
    Nazar<<Al-Wattar<<C#<<93;

---
+++ 93 - Optional Parameters in C Sharp/93 - Optional Parameters in C Sharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+namespace _93___Optional_Parameters_in_C_Sharp
+{
+    class Program
+    {
+        public static void PV(string N = "Youssef Aly", int A = 11)
+        {
+            WriteLine($"Name is {N}, Age is {A}");
+        }
+        static void Main(string[] args)
+        {
+            string N = "Hossam";
+            int A = 34;
+            PV();
+            PV(N);
+            PV(N, A);
+            PV(N: "Aly", A: 52);
+        }
+    }
+}
