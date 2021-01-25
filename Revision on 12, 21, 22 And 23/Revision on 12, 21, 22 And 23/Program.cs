e43a842e4ec1fb3818e90e5f9ceb6f693ff36180
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-01-25 06:59:53 +0200

    Revision on 12, 21, 22, And 23
    Nazar<<Al-Wattar<<C#<<R12,21,22,23;

---
+++ Revision on 12, 21, 22 And 23/Revision on 12, 21, 22 And 23/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Revision_on_12__21__22_And_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //************Revision on L(12)*************
+            //(1) - Console.WriteLine("Hello World");
+            //Console.ReadKey();
+            //************Revision on L(21)*************
+            //(1) - float FM = float.MinValue;
+            //float FX = float.MaxValue;
+            //Console.WriteLine($"Min Value = {FM}, Max Value = {FX}");
+            //******************************************
+            //(2) - float FV = 1F / 3;
+            //double DV = 1D / 3;
+            //decimal MV = 1M / 3;
+            //Console.WriteLine($"float value = {FV}, Double Value = {DV}, Decimal Value = {MV}");
+            //*******Revision on Lessons(22,23)*********
+            //(1) - int CHN = Char.MinValue;
+            //int CHX = Char.MaxValue;
+            //char MV = 'G';
+            //int MVV = 'G';
+            //char MVV2 = Convert.ToChar(MVV);
+            //Console.WriteLine($"Min Char = {CHN}, Max Char = {CHX}, My Character is {MV}, My Characte Number is {MVV}, My Converting is {MVV2}");
+            //******************************************
+            //(2) - string MS = "This is My Variable";
+            //Console.WriteLine($"My String is {MS}");
+            //******************************************
+            //(3) - bool MB = true;
+            //Console.WriteLine($"My Boolean Value is {MB}");
+            //bool MB2 = false;
+            //Console.WriteLine($"My 2ND Boolean Value is {MB2}");
+            //******************************************
+            //(Q1) - int AV = 'A';
+            //Console.WriteLine($"The Character is {A}, And its value is {AV}");
+            //******************************************
+            //(Q2) - int aV = 'a';
+            //Console.WriteLine($"The Character is (a), And the Value is {aV}");
+            //******************************************
+            //(Q3) - string MS = "This is My Character A Value";
+            //Console.WriteLine($"{MS}");
+            //******************************************
+            //(Q4) - string MS2 = "This is My Character a Value";
+            //Console.WriteLine(MS2);
+            //******************************************
+            //(Q5) - int AV = 'A';
+            //int aV = 'a';
+            //string MS = $"This is My Character A Value {AV}";
+            //string MS2 = $"This is My Character a Value {aV}";
+            //Console.WriteLine($"{MS}, {MS2}");
+        }
+    }
+}
