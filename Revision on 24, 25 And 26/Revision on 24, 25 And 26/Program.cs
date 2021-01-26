4802079afc4f316a2c6c79c07baba21b1c2e882d
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-01-26 07:35:58 +0200

    Revision on 24, 25 And 26
    Nazar<<Al-Wattar<<C#<<R24,25,26;

---
+++ Revision on 24, 25 And 26/Revision on 24, 25 And 26/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Revision_on_24__25_And_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //***********Revision on (24)***************
+            //(1) - int MV = -100;
+            //uint MUV = 1000U;
+            //float MFV = 1.5F;
+            //double MDV = 100.5;
+            //decimal MMV = 100.5M;
+            //Console.WriteLine($"{MV}, {MUV}");
+            //Console.WriteLine($"{MMV}, {MDV}");
+            //***********Revision on (25)***************
+            //(2) - string MS = "This is my string var";
+            //Console.WriteLine(MS);
+            //******************************************
+            //(3) - string MS = "Nazar said \"This is args good cours\",And ithink";
+            //Console.WriteLine(MS);
+            //******************************************
+            //(4) - string MS = "Nazar said \'This is args good cours\',And ithink";
+            //Console.WriteLine(MS);
+            //******************************************
+            //(5) - string MS = "Nazar said \\This is args good cours\\,And ithink";
+            //Console.WriteLine(MS);
+            //******************************************
+            //(6) - string MS = "Nazar said This is args good cours, And i think\0";
+            //Console.WriteLine(MS);
+            //******************************************
+            //(7) - string MS = "Nazar said This is args good cours,And ithink\a";
+            //Console.WriteLine(MS);
+            //******************************************
+            //(8) - string MS = "Nazar said This\b is args good cours,And ithink";
+            //Console.WriteLine(MS);
+            //******************************************
+            //(9) - string MS = "Nazar said This\n is args good cours,And ithink";
+            //Console.WriteLine(MS);
+            //******************************************
+            //(10)string MS = "Nazar said This\r is args good cours,And ithink";
+            //Console.WriteLine(MS);
+            //******************************************
+            //(11) - string MS = "Nazar said This is args good cours,And ithink";
+            //string MS2 = @"C:\Program Files\Common Files\Services";
+            //Console.WriteLine(MS);
+            //******************************************
+            //(12) - string MS = "Nazar said This is args good cours,And ithink";
+            //string MS2 = @", Yes it's";
+            //string MS3 = MS + MS2;
+            //Console.WriteLine(MS3);
+            //******************************************
+            //(Q2) - string MS = "Nazar said This is args good cours,And ithink";
+            //Console.WriteLine(MS);
+        }
+    }
+}
