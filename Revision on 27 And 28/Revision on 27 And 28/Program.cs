f9aad41b3a734dc5937e3d3a14ddc1030ab2b83e
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-01-27 07:27:02 +0200

    Revision on 27 And 28;
    Nazar<<Al-Wattar<<C#<<R27,28;

---
+++ Revision on 27 And 28/Revision on 27 And 28/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+namespace Revision_on_27_And_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1) - int MV1 = 15;
+            //int MV2 = 10;
+            //int T = MV1 + MV2;
+            //int T2 = MV1 - MV2;
+            //int T3 = MV1 % MV2;
+            //string MS = "It is Wonderful";
+            //string MS2 = "How is this? ";
+            //MS = MS2 + MS;
+            //int MNV = -10;
+            //int T4 = +MV1;
+            //int T5 = -MV1;
+            //int T6 = +MNV;
+            //WriteLine($"The Result is {T}, Result 2 is {T2}, Result 3 is {T3}, Result 4 is {T4}, Result 5 is {T5}, result 6 is {T6}");
+            //WriteLine(MS);
+            //******************************************
+            //(Q1) - int MV1 = 7, MV2 = 3;
+            //int T = MV1 + MV2;
+            //int T2 = MV1 - MV2;
+            //int T3 = MV1 / MV2;
+            //int T4 = MV1 * MV2;
+            //int T5 = MV1 % MV2;
+            //WriteLine($"Temp 1 is {T}, Temp 2 is {T2}, Temp 3 is {T3}, Temp 4 is {T4}, Temp 5 is {T5}");
+            //******************************************
+            //(Q2) - string MS = "I am ";
+            //string MS2 = "smart";
+            //MS = MS + MS2;
+            //WriteLine($"{MS}");
+        }
+    }
+}   
