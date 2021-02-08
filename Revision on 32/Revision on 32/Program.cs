0a94a989bee28f954c4b5d55a67c09efa54036e4
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-02-08 07:03:10 +0200

    Revision on 32

---
+++ Revision on 32/Revision on 32/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Revision_on_32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
