6c36fac921673e97627d20da8d06fa6f7d62548e
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-02-10 06:56:31 +0200

    Revision on 34 And 35
    Nazar<<Al-Wattar<<C#<<R34,35;

---
+++ Revision on 34 And 36/Revision on 34 And 36/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using static System.Console;
+namespace Revision_on_34_And_36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1) - int MV1 = 9, MV2 = 6;
+            //if (MV1 == 9)
+            //{
+            //    WriteLine("My Var1 == 9");
+            //    if (MV2 == 7)
+            //    {
+            //        WriteLine("My Var2 == 7");
+            //    }
+            //    else
+            //    {
+            //        WriteLine("MV2 is not equal 7");
+            //    }
+            //}
+            //else
+            //{
+            //    WriteLine("MV1 is not equal 9");
+            //}
+            //*****************************************
+            //(Q1) - int MV1 = 10, MV2 = 6, MV3 = 8;
+            //if (MV1 == 10)
+            //{
+            //    WriteLine("MV1 is equal 10");
+            //    if ((MV2 >=5)&&(MV3 <=9))
+            //    {
+            //        WriteLine("The conditions are true");
+            //    }
+            //    else
+            //    {
+            //        WriteLine("The Conditions are false");
+            //    }
+            //}
+            //else
+            //{
+            //    WriteLine("MV1 is not equal 10");
+            //}
+            //******************************************
+            //(2) - int MV = 4, MV2 = 6;
+            //if (MV <= 3 && MV2 >= 5)
+            //{
+            //    WriteLine("Conditions one right");
+            //}
+            //else if (MV <= 4 && MV2 >= 6)
+            //{
+            //    WriteLine("Conditions two  right");
+            //}
+            //else if (MV2 > 5) 
+            //{
+            //    WriteLine("Conditions three right");
+            //}
+            //else
+            //{
+            //    WriteLine("No one of the conditions is true");
+            //}
+        }
+    }
+}
