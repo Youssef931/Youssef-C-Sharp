e2ce420c6c4390e7d5aa566328aeadc93e75916b
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-02-11 07:04:35 +0200

    Revision on 36 And 37
    Nazar<<Al-Wattar<<C#<<R36,37;

---
+++ Revision on  36 And 37/Revision on  36 And 37/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using static System.Console;
+namespace Revision_on__36_And_37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //***********Revision on (36)***************
+            //(1) - string MA = "Cat";
+            //switch (MA)
+            //{
+            //    case "Canary":
+            //        WriteLine("My Animal is canary bird");
+            //        break;
+            //    case "Cat":
+            //        WriteLine("My Animal is cat");
+            //        break;
+            //    case "Camel":
+            //        WriteLine("My Animal is Camel");
+            //        break;
+            //    default:
+            //        WriteLine("No one");
+            //        break;
+            //}
+            //******************************************
+            //(2) - int MV = 15;
+            //switch (MV)
+            //{
+            //    case 10:
+            //        WriteLine("My Var is equal 10");
+            //        break;
+            //    case 7:
+            //        WriteLine("My Var is equal 7");
+            //        break;
+            //    case 15:
+            //        WriteLine("My var is equal 15");
+            //        break;
+            //    default:
+            //        WriteLine("No one");
+            //        break;
+            //}
+            //******************************************
+            //(Q1) - string MS = "Hello";
+            //switch (MS)
+            //{
+            //    case "hello":
+            //        WriteLine("My String is hello");
+            //        break;
+            //    case "Bye":
+            //        WriteLine("My String is Bye");
+            //        break;
+            //    case "welcome":
+            //        WriteLine("My string is welcome");
+            //        break;
+            //    default:
+            //        WriteLine("No one");
+            //        break;
+            //}
+            //**********Revision on (37)****************
+            //(1) - int MV = 9;
+            //string MR = (MV < 10) ? "Less Than 10" : "Greater than 10";
+            //WriteLine($"My Var Result is {MR}");
+            //******************************************
+            //(2) - int MV = 8;
+            //string MR = (MV < 8) ? "Less Than 8" : "Greater than 8";
+            //WriteLine($"My Var Result is {MR}");
+            //******************************************
+            //(3) - string MN = "Nazar", R;
+            //R = (MN.Equals("Nazar")) ? "Yes" : "No";
+            //WriteLine($"My Var Result is {R}");
+            //******************************************
+            //(4) - string MN = "nazar", R;
+            //R = (MN.Equals("Nazar")) ? "Yes" : "No";
+            //WriteLine($"My Var Result is {R}");
+        }
+    }
+}
