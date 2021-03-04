fbd334593e082d4a889c3f086851f466d8969702
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-03-04 06:55:43 +0200

    Revision on 42
    Nazar<<Al-Wattar<<C#<<R42;

---
+++ Revision on 43/Revision on 43/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Revision_on_43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1) - for (int i = 0; i <= 5; i++)
+            //{
+            //    if (i > 2)
+            //        break;
+            //    for (int j = 0; j <= 4; j++)
+            //    {
+            //        Console.WriteLine("\t Hello");
+            //    }
+            //}
+            //******************************************
+            //(2) - for (int i = 0; i <= 5; i++)
+            //{
+            //    //if (i > 2)
+            //    //    break;
+            //    for (int j = 0; j <= 4; j++)
+            //    {
+            //        Console.WriteLine("\t Hello");
+            //    }
+            //}
+            //******************************************
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i>7)
+                {
+                    break;
+                }
+                for (int j = 0; j <= 12; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                }
+            }
+        }
+    }
+}
