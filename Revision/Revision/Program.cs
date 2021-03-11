2019d35bd71433812e8ab6b2d64fa1cb5203a174
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-03-11 06:13:41 +0200

    Revision

---
+++ Revision/Revision/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+namespace Revision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1) - for (int i = 0; i <= 5; i++)
+            //{
+            //    WriteLine("Hello Nazar");
+            //    if (i > 2)
+            //    {
+            //        continue;
+            //    }
+            //    for (int j = 0; j <= 4; j++)
+            //    {
+            //      WriteLine("\t Hello world");
+            //    }
+            //    WriteLine("Hello Nazar");
+            //   WriteLine("\n");
+            //}
+            //********************************************
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 7)
+                {
+                    continue;
+                }
+                for (int j = 0; j < 10; j++)
+                {
+                   WriteLine($"{i} * {j} = {i * j}");
+                }
+            }
+        }
+    }
+}
