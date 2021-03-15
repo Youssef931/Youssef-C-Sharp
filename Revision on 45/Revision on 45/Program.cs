2ab2f02fe630e35797a29e861828b817cb51653e
Youssef931 <69377647+Youssef931@users.noreply.github.com>
2021-03-15 06:34:41 +0200

    Revsision on 45

---
+++ Revision on 45/Revision on 45/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Revision_on_45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long MV = 1000;
+            float MV2 = MV;
+            float MFV = 12.5f;
+            //int MIV2 = MFV;
+            Console.WriteLine(MV2);
+            char MV3 = 'G';
+            int MIV = MV3;
+            Console.WriteLine(MIV);
+        }
+    }
+}
