ce379462a0ace2c430710efe3b3edb5dd3f6c46f
Youssef931 <youssefaly2009@gmail.com>
2021-09-21 01:01:15 +0200

    Watermelon

---
+++ Watermelon/Watermelon/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Watermelon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w = 0;
+            Console.ReadLine();
+            if (w % 2==0 && w>2)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
